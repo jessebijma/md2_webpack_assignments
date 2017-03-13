53df93cbeba8d131a2b0c1fe1b6a7137ac91839c
Jesse <jesseebijma@gmail.com>
2017-03-13 14:01:43 +0100

    commit 1

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
 /**
  * Opdracht 1: primitives
  * Maak voor de volgende primitives 5 goed bedachte variabelen per primitive
- * 
+
  * 5 variabelen met daarin een string
  * 5 variabelen met daarin een number
  * 5 variabelen met daarin een boolean
@@ -18,6 +18,24 @@
  * Bijvoorbeeld een string variabele met de naam 'teacherName' en als waarde 'Berend'
  */
 
+var teacherName1 = "Berend";
+var teacherName2 = "Theo";
+var teacherName3 = "Hjalmar";
+var teacherName4 = "Ed";
+var teacherName5 = "Rosmerta";
+
+var teacherAge1 = 31;
+var teacherAge2 = 200;
+var teacherAge3 = 38;
+var teacherAge4 = 58;
+var teacherAge5 = 55;
+
+
+var isProgramming = true;
+var isDrinking = false;
+var isEating = true;
+var isNotProgramming = false;
+var bool5 = true;
 /**
  * Opdracht 2: Arrays
  * Bedenk 3 arrays en vul ze met primitives
@@ -25,33 +43,58 @@
  *
  * Bijvoorbeeld een Array met de naam 'students' en als waarde ['Berend', "Erwin']
  */
-
+var teacherNames = [teacherName1,teacherName2,teacherName3,teacherName4,teacherName5];
+var teacherAges = [teacherAge1,teacherAge2,teacherAge3,teacherAge4,teacherAge5];
+var bools = [isDrinking,isEating,isNotProgramming,isProgramming,bool5];
 /**
  * Opdracht 3: Arrays push
  * Maak een lege array aan
  * push er een string in
  */
-
+var emptyArray = [];
+emptyArray.push("test");
 /**
  * Opdracht 4: Array splice
  * Maak een array aan en vul hem met namen
  * Gooi de naam op de 1e plek weg
  * Gooi de naam op de 3e plek weg
  */
-
+var names = ["name1","name2","name3","name4"];
+names.splice(0,1);
+names.splice(1,1);
 /**
  * Opdracht 5: Objects
  * Maak van 10 objecten uit je dagelijkse leven een object
  *
  * Bijvoorbeeld een object met de gegevens van je mobiel zoals welk merk, type en hoe oud hij is
  */
-
+var car = {type:"Fiat", model:"500", color:"White"};
+var bike = {type:"Gazelle", model:"450", color:"grey", age:"2"};
+var laptop = {type:"Dell", model:"Latitude 3550", color:"Black"};
+var bag = {brand:"Patta", collection:"Running Team", color:"Black/Pink/Purple Camo"};
 /**
  * Opdracht 6: Functions
  * Bedenk 4 functies die handig zouden kunnen zijn
  * Bijvoorbeeld een functie met de naam 'multiply'. Deze functie verwacht 2 parameters value & multiplier
  * console.log( value * multiplier)
  */
+const myFunct = (a,b) => {
+        return a * b;
+};
+
+console.log(myFunct(a,b));
+
+const myFunct1 = (c,d) => {
+    return c + d;
+};
+
+console.log(myFunct1(c,d));
+
+const myFunct2 = (e,f) => {
+    return e - f;
+};
+
+console.log(myFunct2(e,f));
 
 /**
  * Opdracht 7: Function & objects
@@ -59,7 +102,12 @@
  * vervolgens returned de functie een object met daarin de firstname en lastname opgeslagen
  * roep deze functie aan en console.log() de waarde
  */
+    const myFunct3 = (firstName, lastName) => {
+    let object = {firstName : "test", lastName : "test"};
+    return object;
+}
 
+console.log(myFunct3());
 /**
  * Opdracht 8: Functions, objects
  * Maak een object genaamd 'student'
